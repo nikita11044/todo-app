7989dffea9af7094518bdc78aa6d9f0d45a59f64
nikita11044 <nikita11042000@gmail.com>
2021-09-03 23:41:21 +0300

    improved architecture

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,17 @@ import (
 
 	"github.com/nikita11044/todo-app"
 	"github.com/nikita11044/todo-app/pkg/handler"
+	"github.com/nikita11044/todo-app/pkg/repository"
+	"github.com/nikita11044/todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repo := repository.NewRepository()
+	service := service.NewService(repo)
+	handler := handler.NewHandler(service)
+	
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running hhtp server: %s", err.Error())
 	}
 }
